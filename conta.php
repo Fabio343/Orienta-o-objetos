b91ac2feb5b79d580932339a2b8adecf2d138505
php 12 <php.12@lampadinha.cec.ime.usp.br>
2018-01-19 22:22:12 -0200

    classe conta

---
+++ conta.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *
+ */
+class conta
+{
+  public $numero ;
+  public $saldo;
+}
+ $c = new conta();
+ $c -> numero =123;
+ $c -> saldo = 1000;
+
+ echo "Número:" $c-> numero.PHP_EOL;
+ echo "Saldo:" $c-> saldo.PHP_EOL;
+ ?>
