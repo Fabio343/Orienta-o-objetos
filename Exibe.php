350a0d20361430b4d056d455b0f0f38dce6c72ef
php 12 <php.12@jerry.cec.ime.usp.br>
2018-01-22 21:45:05 -0200

    Primeiro formulario de cadastro

---
+++ Exibe.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exibe as informaçẽos</title>
+  </head>
+  <body>
+    <?php
+      require 'cliente2.php';
+
+      $cliente=new Cliente();
+      $cliente->nome=$_POST['nome'];
+      $cliente->email=$_POST['email'];
+
+     ?>
+    <h2>Cliente</h2>
+
+    <h4>Nome:<? $cliente->nome ?></h4>
+    <h4>Email:<? $cliente->email ?></h4>
+
+  </body>
+</html>
