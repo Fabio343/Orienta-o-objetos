abc241785f450194e00bf9d9fac8f3f77ed7f64d
php 12 <php.12@jerry.cec.ime.usp.br>
2018-01-22 21:00:21 -0200

    Formatando data com teste da classe cartão de credito 2

--- cartaodecredito.php
+++ cartaodecredito.php
@@ -2,9 +2,7 @@
  /**
   *
   */
-require 'lib/Carbon.php'; //chamando um arquivo
 
-use Carbon\Carbon; // chamando uma função do arquivo anterior
 
 
  class cartao
@@ -14,22 +12,26 @@ use Carbon\Carbon; // chamando uma função do arquivo anterior
 
  }
 
+ require 'lib/Carbon.php'; //chamando um arquivo
+
+ use Carbon\Carbon; // chamando uma função do arquivo anterior
+
 $cartao1=new cartao;
 $cartao2=new cartao;
 
 $cartao1->numero=111111;
-$cartao1->validade=Carbon::createFromDate(2013,1,1, 'American/Sao_Paulo');
+$cartao1->validade=Carbon::createFromDate(2013, 1, 1, 'America/Sao_Paulo');
 
 $cartao2->numero=222222;
-$cartao2->validade=Carbon::createFromDate(2014,1,1,'American/Sao_Paulo');
+$cartao2->validade=Carbon::createFromDate(2014, 1, 1,'America/Sao_Paulo');
 
 echo "Imprimindo dos dados <br>";
 
 echo "Numero do cartão 1: $cartao1->numero <br>".PHP_EOL;
-echo "data de validade do cartão  1: {$cartao1->validade format(d,m,y)} <br>".PHP_EOL;
+echo "data de validade do cartão  1: {$cartao1->validade->format('d/m/y')} <br>".PHP_EOL;
 
 echo "Numero do cartão 2: $cartao2->numero <br>".PHP_EOL;
-echo "data de validade do cartão  2: $cartao2->validade <br>".PHP_EOL;
+echo "data de validade do cartão  2: {$cartao2->validade->format('d/m/y')} <br>".PHP_EOL;
 
 
  ?>
