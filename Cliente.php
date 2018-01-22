fe317bca661327b2868c34488677eadbe09d38ca
php 12 <php.12@jerry.cec.ime.usp.br>
2018-01-22 19:49:10 -0200

    classe cliente

--- Cliente.php
+++ Cliente.php
@@ -1,12 +1,27 @@
 <?php
 /**
  *
- */
+ */ //criando dois objetos clientes  com a classe definida por nome e codigo
 class Cliente
 {
   public $nome;
   public $codigo;
 }
 
+$client1= new Cliente();
+
+$client1->nome="Rafael Cosentino";
+$client1->codigo="codigo 1";
+
+$client2= new Cliente();
+
+$client2->nome="Jonas Hirata";
+$client2->codigo="codigo 2";
+
+echo "Nome do cliente 1: $client1->nome".PHP_EOL;
+echo "codigo do cliente 1: $client1->codigo".PHP_EOL;
+
+echo "Nome do cliente 2: $client2->nome".PHP_EOL;
+echo "codigo do cliente 2: $client2->codigo".PHP_EOL;
 
  ?>
